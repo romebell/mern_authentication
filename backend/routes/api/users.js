e7ccf10d5daa7e4dab506596121e24c26c0ff674
Rome Bell <romebell5@gmail.com>
2020-09-03 13:58:21 -0700

    [users] Add /login route and test

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -48,4 +48,38 @@ router.post('/register', (req, res) => {
   })
 });
 
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find a user via email
+  db.User.findOne({ email })
+  .then(user => {
+    if (!user) {
+      res.status(400).json({ msg: 'User not found'});
+    } else {
+      // Check password with bcrypt
+      bcrypt.compare(password, user.password)
+      .then(isMatch => {
+        if (isMatch) {
+          // User match, send JSON web token
+          // Create a token payload (you can include anything you want)
+          const payload = {
+            id: user.id,
+            name: user.name,
+            email: user.email
+          };
+
+          // Sign token
+          jwt.sign(payload, JWT_SECRET, { expiresIn: 3600 }, (error, token) => {
+            res.json({ success: true, token: `Bearer ${token}` });
+          });
+        } else {
+          return res.status(400).json({ password: 'Password or email is incorrect' });
+        }
+      });
+    }
+  });
+});
+
 module.exports = router;
