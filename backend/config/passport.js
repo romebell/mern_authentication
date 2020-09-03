d6974d2d16ada82aa049a10c7eeaff57354c2329
Rome Bell <romebell5@gmail.com>
2020-09-03 10:00:33 -0700

    [passport] Set env variable, set up options, and export passport function

---
+++ backend/config/passport.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+// A passport strategy for authenicating with a JSON Web Token
+// This allows to authenicate endpoints using the token.
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+const options = {}
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = process.env.JWT_SECRET;
+
+module.exports = (passport) => {
+  passport.use(new JwtStrategy(options, (jwt_payload, done) => {
+    User.findById(jwt_payload.id)
+    .then(user => {
+      if (user) {
+        // If the user is found, return null (for error) and user
+        return done(null, user);
+      } else {
+        // If no user is found
+        return done(null, false);
+      }
+    })
+    .catch(error => console.log(error));
+  }));
+}
--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,15 @@ const cors = require('cors');
 const port = process.env.PORT || 8000;
 const passport = require('passport');
 
-
+// Middleware
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+// Importing passport file into server
+require('./config/passport')(passport);
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Smile, you are being watch by the Backend Team' });
