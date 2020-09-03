9c340ddf426ae4004899881e10706a8e5d61ab79
Rome Bell <romebell5@gmail.com>
2020-09-03 15:08:13 -0700

    [components] Add About and Footer components

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Welcome from './components/Welcome';
+import About from './components/About';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
