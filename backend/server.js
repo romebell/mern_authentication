0bcf5783267db27adbefec45a5c0728e878ea244
Rome Bell <romebell5@gmail.com>
2020-09-03 09:31:03 -0700

    [server] Set up and test home route

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 8000;
+const passport = require('passport');
+
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Smile, you are being watch by the Backend Team' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
