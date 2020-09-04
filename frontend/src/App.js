3618fce5b2978106b13abe240af5275f140b1116
Rome Bell <romebell5@gmail.com>
2020-09-03 17:16:20 -0700

    [signup] Add component and test in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import Navbar from './components/Navbar';
 import Signup from './components/Signup';
-import Login from './components/Login';
-import Profile from './components/Profile';
+// import Login from './components/Login';
+// import Profile from './components/Profile';
 import Welcome from './components/Welcome';
 import About from './components/About';
 import Footer from './components/Footer';
@@ -59,12 +59,12 @@ function App() {
       <div className="container mt-5">
         <Switch>
           <Route path="/signup" component={ Signup } />
-          <Route 
+          {/* <Route 
             path="/login" 
             render={ (props) => <Login {...props} nowCurrentUser={nowCurrentUser} setIsAuthenticated={setIsAuthenticated} user={currentUser}/>} 
-          />
+          /> */}
           <Route path="/about" component={ About } />
-          <PrivateRoute path="/profile" component={ Profile } user={currentUser} />
+          {/* <PrivateRoute path="/profile" component={ Profile } user={currentUser} /> */}
           <Route exact path="/" component={ Welcome } />
         </Switch>
       </div>
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
+const Signup = () => {
+    let [name, setName] = useState('');
+    let [email, setEmail] = useState('')
+    let [password, setPassword] = useState('');
+    let [confirmPassword, setConfirmPassword] = useState('');
+    let [redirect, setRedirect] = useState(false);
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (password === confirmPassword) {
+            const newUser = { name, email, password }
+
+            axios.post(`${REACT_APP_SERVER_URL}/api/users/register`, newUser)
+            .then(response => {
+                console.log(response);
+                setRedirect(true);
+            })
+            .catch(error => console.log(error));
+        }
+    }
+
+    if (redirect) return <Redirect to="/login" />
+
+    return (
+        <div className="row mt-4">
+            <div className="col-md-7 offset-md-3">
+                <div className="card card-body">
+                    <h2 className="py-2">Signup</h2>
+                    <form action="/ideas" method="POST" onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" name="name" value={name} onChange={handleName} className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" value={email} onChange={handleEmail} className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" value={password} onChange={handlePassword} className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="confirmPassword">Confirm Password</label>
+                            <input type="password" name="confirmPassword" value={confirmPassword} onChange={handleConfirmPassword} className="form-control"/>
+                        </div>
+                        <button type="submit" className="btn btn-primary float-right">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default Signup;
