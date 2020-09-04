cdcf122916d1e0109cb9dc3d913cd38c4fe250c0
Rome Bell <romebell5@gmail.com>
2020-09-04 10:55:49 -0700

    [component] Add Profile and test in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import setAuthToken from './utils/setAuthToken';
 import Navbar from './components/Navbar';
 import Signup from './components/Signup';
 import Login from './components/Login';
-// import Profile from './components/Profile';
+import Profile from './components/Profile';
 import Welcome from './components/Welcome';
 import About from './components/About';
 import Footer from './components/Footer';
@@ -64,7 +64,7 @@ function App() {
             render={ (props) => <Login {...props} nowCurrentUser={nowCurrentUser} setIsAuthenticated={setIsAuthenticated} user={currentUser}/>} 
           />
           <Route path="/about" component={ About } />
-          {/* <PrivateRoute path="/profile" component={ Profile } user={currentUser} /> */}
+          <PrivateRoute path="/profile" component={ Profile } user={currentUser} />
           <Route exact path="/" component={ Welcome } />
         </Switch>
       </div>
---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Profile = (props) => {
+    console.log(props);
+    const userData = props.user ? 
+    (<div>
+        <h1>Profile</h1>
+        <p><strong>Name:</strong> {props.user.name}</p> 
+        <p><strong>Email:</strong> {props.user.email}</p> 
+        <p><strong>ID:</strong> {props.user.id}</p> 
+    </div>) : <h4>Loading...</h4>
+
+    const errorDiv = () => {
+        return (
+            <div className="text-center pt-4">
+                <h3>Please <Link to="/login">login</Link> to view this page</h3>
+            </div>
+        );
+    };
+    
+    return (
+        <div>
+            { props.user ? userData : errorDiv() }
+        </div>
+    );
+
+}
+
+export default Profile;
